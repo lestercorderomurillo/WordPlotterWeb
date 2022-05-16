c37437c79d1abb7af45dbff5e47a44898a8711b1
Lester Cordero Murillo <lestercorderomurillo@gmail.com>
2022-05-16 16:33:35 -0600

    Manejo de la lista de archivos en la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Dialog } from './dialog.component';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,9 @@ import { Dialog } from './dialog.component';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  
   formGroup: any;
-  registerGroups: Array<any> = [];
+  registeredGroups: Array<any> = [];
+  registeredFiles: Array<Array<any>> = [];
 
   constructor(public dialog: MatDialog) {}
 
@@ -19,26 +20,63 @@ export class AppComponent implements OnInit {
     this.formGroup = new FormGroup({
       groupName: new FormControl('', [Validators.required]),
     });
+
+    this.onClickSubmitForm('Grupo01');
+    this.onClickSubmitForm('Grupo02');
+    this.onClickSubmitForm('Grupo03');
+    this.onClickSubmitForm('Grupo04');
+    this.onClickSubmitForm('Grupo05');
+    this.onClickSubmitForm('Grupo06');
+    this.onClickSubmitForm('Grupo07');
+    this.onClickSubmitForm('Grupo08');
+    this.onClickSubmitForm('Grupo09');
+    this.onClickSubmitForm('Grupo10');
+    this.onClickSubmitForm('Grupo11');
   }
 
-  onClickSubmit(groupName: any) {
-    if (!this.registerGroups.includes(groupName)) {
-      this.registerGroups.push(groupName);
+  onClickSubmitForm(groupName: any) {
+    if (!this.registeredGroups.includes(groupName)) {
+      this.registeredGroups.push(groupName);
     } else {
       this.dialog.open(Dialog, {
         data: {
-          title: 'Error',
-          message: 'The group already exists. Please use another name for that group.',
+          title: 'Oops, error!',
+          message:
+            'The group already exists. Please use another name for that group.',
         },
       });
     }
+  }
+
+  onFileChange(groupName: any, event: any) {
+    this.registeredFiles[groupName] = event.target.files;
+
+    console.log(this.registeredFiles[groupName]);
   }
 
   onClickCloseDialog() {
     this.dialog.closeAll();
   }
 
-  onClickDelete(groupName: any) {
-    this.registerGroups = this.registerGroups.filter((e) => e !== groupName);
+  onClickDeleteGroup(groupName: any) {
+    this.registeredGroups = this.registeredGroups.filter(
+      (e) => e !== groupName
+    );
+  }
+
+  onClickAddFilesToGroup(groupName: any) {
+    let element: HTMLElement = document.getElementById(
+      'u_' + this.md5(groupName)
+    ) as HTMLElement;
+
+    element.click();
+  }
+
+  md5(groupName: any) {
+    return Md5.hashStr(groupName);
+  }
+
+  trackItem (index: number, item: any) {
+    return item.trackId;
   }
 }
